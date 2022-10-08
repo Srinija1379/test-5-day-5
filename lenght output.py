d85dba9527f58a649d693bdb7966a26e71240e0e
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-08 08:44:15 +0530

    Add files via upload

---
+++ lenght output.py
@@ -0,0 +1,27 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\length day 4.py 
+enter string:hello world
+helo wrd
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\length day 4.py 
+enter string:malayalam
+maly
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\length day 4.py 
+enter string:srinija
+srinja
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\length day 4.py 
+enter string:dolly
+doly
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\length day 4.py 
+enter string:tommorrow
+tomrw
+>>> 
+ RESTART: C:\Users\sse\AppData\Local\Programs\Python\Python37-32\length day 4.py 
+enter string:yellow
+yelow
+>>> 
