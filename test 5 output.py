38638291e3123596d87f1c36b9d43783f74b0e3d
Srinija1379 <112627711+Srinija1379@users.noreply.github.com>
2022-10-08 09:04:08 +0530

    Add files via upload

---
+++ test 5 output.py
@@ -0,0 +1,30 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/test 5.py =
+Enter the string: hello world
+Enter a character to be deleted: l
+String after character removed:  heo word
+>>> 
+= RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/test 5.py =
+Enter the string: malayalam
+Enter a character to be deleted: m
+String after character removed:  alayala
+>>> 
+= RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/test 5.py =
+Enter the string: 123456789
+Enter a character to be deleted: 678
+String after character removed:  123456789
+>>> 
+= RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/test 5.py =
+Enter the string: sreeee
+Enter a character to be deleted: e
+String after character removed:  sr
+>>> 
+= RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/test 5.py =
+Enter the string: python
+Enter a character to be deleted: p
+String after character removed:  ython
+>>> 
+= RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/test 5.py =
+Enter the string: 
